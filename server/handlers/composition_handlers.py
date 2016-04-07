b4c34d501dcad34e178d3307bc7d61c05d5d2bee
CJ Carey <perimosocordiae@gmail.com>
2016-04-07 14:06:07 -0400

    Fixing mol plots

--- server/handlers/composition_handlers.py
+++ server/handlers/composition_handlers.py
@@ -10,9 +10,10 @@ from ..web_datasets import DATASETS
 from .base import BaseHandler
 
 MOL_DATA = {
+  # Oxides
   'Al2O3': (0.01962, 'Al'),
   'CaO': (0.01783, 'Ca'),
-  'Fe2O3': (0.01392, 'Fe'),  # ????
+  'Fe2O3': (0.012524, 'Fe'),
   'FeOT': (0.01392, 'Fe'),
   'K2O': (0.02123, 'K'),
   'MgO': (0.02481, 'Mg'),
@@ -20,6 +21,72 @@ MOL_DATA = {
   'Na2O': (0.03227, 'Na'),
   'SiO2': (0.01664, 'Si'),
   'TiO2': (0.01252, 'Ti'),
+  'CO2': (0.0227, 'C'),
+  'H2O': (0.1110, 'H'),
+  'SO3': (0.0125, 'S'),
+  # Trace elements
+  'Ag': (1e-4/107.8682, 'Ag'),
+  'As': (1e-4/74.9216, 'As'),
+  'Au': (1e-4/196.96655, 'Au'),
+  'B': (1e-4/10.81, 'B'),
+  'Ba': (1e-4/137.327, 'Ba'),
+  'Be': (1e-4/9.012, 'Be'),
+  'Bi': (1e-4/208.98040, 'Bi'),
+  'Br': (1e-4/79.904, 'Br'),
+  'Cd': (1e-4/112.414, 'Cd'),
+  'Ce': (1e-4/140.116, 'Ce'),
+  'Cl': (1/35.4527, 'Cl'),
+  'Co': (1e-4/58.9332, 'Co'),
+  'Cr': (1e-4/51.9961, 'Cr'),
+  'Cs': (1e-4/132.90545, 'Cs'),
+  'Cu': (1e-4/63.546, 'Cu'),
+  'Dy': (1e-4/162.5, 'Dy'),
+  'Er': (1e-4/67.26, 'Er'),
+  'Eu': (1e-4/151.964, 'Eu'),
+  'F': (1/18.9984023, 'F'),
+  'Ga': (1e-4/69.723, 'Ga'),
+  'Gd': (1e-4/157.25, 'Gd'),
+  'Ge': (1e-4/72.61, 'Ge'),
+  'Hf': (1e-4/178.49, 'Hf'),
+  'Hg': (1e-4/200.592, 'Hg'),
+  'Ho': (1e-4/164.93032, 'Ho'),
+  'I': (1e-4/126.90447, 'I'),
+  'In': (1e-4/114.818, 'In'),
+  'Ir': (1e-4/192.21, 'Ir'),
+  'La': (1e-4/138.9055, 'La'),
+  'Li': (1e-4/6.941, 'Li'),
+  'Lu': (1e-4/174.967, 'Lu'),
+  'Mn': (1e-4/54.938049, 'Mn'),
+  'Mo': (1e-4/95.94, 'Mo'),
+  'N': (1e-4/14.007, 'N'),
+  'Nb': (1e-4/92.906, 'Nb'),
+  'Nd': (1e-4/144.24, 'Nd'),
+  'Ni': (1e-4/58.6934, 'Ni'),
+  'P': (1e-4/30.973762, 'P'),
+  'Pb': (1e-4/207.2, 'Pb'),
+  'Pd': (1e-4/106.42, 'Pd'),
+  'Pr': (1e-4/140.90765, 'Pr'),
+  'Pt': (1e-4/195.084, 'Pt'),
+  'Rb': (1e-4/85.4678, 'Rb'),
+  'Sb': (1e-4/121.76, 'Sb'),
+  'Sc': (1e-4/44.95591, 'Sc'),
+  'Se': (1e-4/78.971, 'Se'),
+  'Sm': (1e-4/150.36, 'Sm'),
+  'Sn': (1e-4/118.71, 'Sn'),
+  'Sr': (1e-4/87.62, 'Sr'),
+  'Ta': (1e-4/180.9479, 'Ta'),
+  'Tb': (1e-4/58.92534, 'Tb'),
+  'Te': (1e-4/127.60, 'Te'),
+  'Th': (1e-4/232.0381, 'Th'),
+  'Tl': (1e-4/47.867, 'Tl'),
+  'Tm': (1e-4/168.93421, 'Tm'),
+  'U': (1e-4/238.0289, 'U'),
+  'V': (1e-4/50.9415, 'V'),
+  'W': (1e-4/183.84, 'W'),
+  'Y': (1e-4/88.90585, 'Y'),
+  'Yb': (1e-4/173.04, 'Yb'),
+  'Zn': (1e-4/65.39, 'Zn'),
+  'Zr': (1e-4/91.224, 'Zr'),
 }
 
 
@@ -89,17 +156,10 @@ class CompositionPlotHandler(BaseHandler):
     x_keys = [k.split('$',1) for k in x_input.split('+')] if x_input else []
     y_keys = [k.split('$',1) for k in y_input.split('+')] if y_input else []
     use_group_name = len(set(k[0] for k in (x_keys + y_keys))) > 1
-    x_comps, x_labels = comps_with_labels(ds, x_keys, use_group_name, use_mols)
-    y_comps, y_labels = comps_with_labels(ds, y_keys, use_group_name, use_mols)
-
+    do_sum = x_keys and y_keys
     mask = fig_data.filter_mask[ds]
-
-    if x_comps and y_comps:
-      x_data = sum(m.get_array(mask) for m in x_comps)
-      y_data = sum(m.get_array(mask) for m in y_comps)
-    else:
-      x_data = [m.get_array(mask) for m in x_comps]
-      y_data = [m.get_array(mask) for m in y_comps]
+    x_data, x_labels = comps_with_labels(ds, mask, x_keys, use_group_name, use_mols, do_sum)
+    y_data, y_labels = comps_with_labels(ds, mask, y_keys, use_group_name, use_mols, do_sum)
 
     if do_fit:
       # handle NaNs
@@ -112,15 +172,15 @@ class CompositionPlotHandler(BaseHandler):
                    x_data.shape[0], x_labels, y_labels)
 
       # compute line of best fit
-      # slope, yint, rval, _, stderr = linregress(x_data, y_data)
-      # xint = -yint / slope
       rval, _ = scipy.stats.pearsonr(x_data, y_data)
       model = odr.ODR(odr.Data(x_data, y_data), odr.unilinear)
       result = model.run()
       slope, yint = result.beta
       slope_err, yint_err = result.sd_beta
+      xint = -yint / slope
+      xint_err = np.abs(xint) * np.linalg.norm([yint_err/yint, slope_err/slope])
       results = dict(rval=rval**2, slope=slope, slope_err=slope_err,
-                     yint=yint, yint_err=yint_err)
+                     yint=yint, yint_err=yint_err, xint=xint, xint_err=xint_err)
 
       # make coordinates for the fit line
       x_min, x_max = x_data.min(), x_data.max()
@@ -138,7 +198,7 @@ class CompositionPlotHandler(BaseHandler):
       key, _ = (x_keys if x_keys else y_keys)[0]
       ax.set_title(ds.metadata[key].display_name(key))
 
-    if x_comps and y_comps:
+    if do_sum:
       # scatter plot: x vs y
       color_key = self.get_argument('color_by')
       color_meta = ds.metadata.get(color_key, None)
@@ -152,7 +212,7 @@ class CompositionPlotHandler(BaseHandler):
       suffix = ' (moles)' if use_mols else ''
       ax.set_xlabel(' + '.join(x_labels) + suffix)
       ax.set_ylabel(' + '.join(y_labels) + suffix)
-    elif x_comps:
+    elif x_keys:
       # histogram along x
       ax.hist(x_data, bins='auto', orientation='vertical', label=x_labels)
       fig_data.hist_data = x_data
@@ -181,20 +241,24 @@ class CompositionPlotHandler(BaseHandler):
     return self.write(json_encode(results))
 
 
-def comps_with_labels(ds, comp_keys, use_group_name, use_mols):
-  comps, labels = [], []
+def comps_with_labels(ds, mask, comp_keys, use_group_name, use_mols, do_sum):
+  tmp, labels = [], []
   for k1, k2 in comp_keys:
     m1 = ds.metadata[k1]
     m2 = m1.comps[k2]
-    comps.append(m2)
     name = m2.display_name(k2)
-    if use_mols and 'O' in name:
-      name = MOL_DATA[name][1]
-    if use_group_name:
-      labels.append('(%s: %s)' % (m1.display_name(k1), name))
+    if use_mols:
+      factor, name = MOL_DATA[name]
     else:
-      labels.append(name)
-  return comps, labels
+      factor = 1
+    if use_group_name:
+      name = '(%s: %s)' % (m1.display_name(k1), name)
+    tmp.append((m2, factor))
+    labels.append(name)
+
+  convert_fn = sum if do_sum else list
+  data = convert_fn(f * meta.get_array(mask) for meta, f in tmp)
+  return data, labels
 
 
 routes = [
