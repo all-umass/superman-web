1122fb274316d5e2aced052afa87fc7321051aaf
CJ Carey <perimosocordiae@gmail.com>
2016-09-15 16:58:56 -0400

    Allow zip-file traj data upload

--- server/handlers/import_handlers.py
+++ server/handlers/import_handlers.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import
 import logging
 import numpy as np
 from io import BytesIO
+from superman.file_io import parse_spectrum
 from tornado.escape import url_escape
+from zipfile import is_zipfile, ZipFile
 
 from .base import BaseHandler
 from ..web_datasets import (
-    WebVectorDataset, WebLIBSDataset, DATASETS, PrimaryKeyMetadata,
-    NumericMetadata, BooleanMetadata, LookupMetadata)
+    WebTrajDataset, WebVectorDataset, WebLIBSDataset, DATASETS,
+    PrimaryKeyMetadata, NumericMetadata, BooleanMetadata, LookupMetadata)
 
 
 class DatasetImportHandler(BaseHandler):
@@ -16,60 +18,91 @@ class DatasetImportHandler(BaseHandler):
     ds_kind = self.get_argument('ds_kind')
 
     if ds_kind not in DATASETS:
-      logging.error('Invalid ds_kind: %r', ds_kind)
-      self.set_status(400)
-      return self.finish('Invalid dataset kind.')
+      return self._raise_error(400, 'Invalid dataset kind.',
+                               'Invalid ds_kind: %r' % ds_kind)
 
     if ds_name in DATASETS[ds_kind]:
-      logging.error('import would clobber existing: %s [%s]', ds_name, ds_kind)
-      self.set_status(403)
-      return self.finish('Dataset already exists.')
+      return self._raise_error(
+          403, 'Dataset already exists.',
+          'ds import would clobber existing: %s [%s]' % (ds_name, ds_kind))
 
     if not self.request.files or 'spectra' not in self.request.files:
-      logging.error('no spectrum data uploaded')
-      self.set_status(400)
-      return self.finish('No spectrum data uploaded.')
+      return self._raise_error(400, 'No spectrum data uploaded.')
+
+    meta_kwargs, meta_pkeys = self._load_metadata_csv()
+    if meta_kwargs is None:
+      return
 
     f, = self.request.files['spectra']
     fh = BytesIO(f['body'])
+    if is_zipfile(fh):
+      # interpret this as a ZIP of csv files
+      success = self._traj_ds(fh, ds_name, ds_kind, meta_kwargs, meta_pkeys)
+    else:
+      # this is one single csv file with all spectra in it
+      success = self._vector_ds(fh, ds_name, ds_kind, meta_kwargs, meta_pkeys)
+
+    if success:
+      self.write('/explorer?ds_kind=%s&ds_name=%s' % (
+        ds_kind, url_escape(ds_name, plus=False)))
+
+  def _raise_error(self, status_code, user_msg, log_msg=None):
+    if log_msg is None:
+      logging.error(user_msg)
+    else:
+      logging.error(log_msg)
+    self.set_status(status_code)
+    self.finish(user_msg)
+    return False
+
+  def _traj_ds(self, fh, ds_name, ds_kind, meta_kwargs, meta_pkeys):
+    zf = ZipFile(fh)
+    traj_data = {}
+    for subfile in zf.infolist():
+      if subfile.file_size <= 0:
+        continue
+      fname = subfile.filename
+      # read and wrap, because the ZipExtFile object isn't seekable
+      sub_fh = BytesIO(zf.open(subfile).read())
+      try:
+        traj_data[fname] = parse_spectrum(sub_fh)
+      except Exception as e:
+        return self._raise_error(
+            415, 'Unable to parse spectrum file: %s' % fname,
+            'bad spectrum subfile (%s): %s' % (fname, e))
+
+    if len(meta_pkeys) > 0 and len(meta_pkeys) != len(traj_data):
+      return self._raise_error(415, 'Spectrum and metadata names mismatch.',
+                               'wrong number of meta_pkeys for traj')
+    for pkey in meta_pkeys:
+      if pkey not in traj_data:
+        return self._raise_error(415, 'Spectrum and metadata names mismatch.',
+                                 'extra meta_pkey not in traj_data: %r' % pkey)
+    if len(meta_pkeys) == 0:
+      meta_pkeys = traj_data.keys()
+
+    def _load(ds):
+      ds.set_data(meta_pkeys, traj_data, **meta_kwargs)
+      return True
+
+    WebTrajDataset(ds_name, ds_kind, _load)
+    return True
+
+  def _vector_ds(self, fh, ds_name, ds_kind, meta_kwargs, meta_pkeys):
     try:
       data = np.genfromtxt(fh, dtype=np.float32, delimiter=',', names=True)
       wave = data[data.dtype.names[0]]
       spectra = data.view((np.float32, len(data.dtype.names))).T[1:]
     except Exception as e:
-      logging.error('bad spectra file: %s', e)
-      self.set_status(415)
-      return self.finish('Unable to parse spectrum data CSV.')
+      return self._raise_error(415, 'Unable to parse spectrum data CSV.',
+                               'bad spectra file: %s' % e)
+
+    if ds_kind == 'LIBS' and wave.shape != (6144,):
+      return self._raise_error(415, 'Wrong number of channels for LIBS data.')
 
     pkey = np.array(data.dtype.names[1:])
-
-    # metadata is optional
-    meta_kwargs = {}
-    if 'metadata' in self.request.files:
-      f, = self.request.files['metadata']
-      fh = BytesIO(f['body'])
-      try:
-        meta = np.genfromtxt(fh, dtype=None, delimiter=',', names=True)
-        meta_pkeys = np.array(meta[meta.dtype.names[0]])
-      except Exception as e:
-        logging.error('bad metadata file: %s', e)
-        self.set_status(415)
-        return self.finish('Unable to parse metadata CSV.')
-
-      if (meta_pkeys != pkey).any():
-        logging.error('mismatching meta_pkeys')
-        self.set_status(415)
-        return self.finish('Spectrum and metadata names mismatch.')
-
-      for meta_name in meta.dtype.names[1:]:
-        x = meta[meta_name]
-        if np.issubdtype(x.dtype, np.bool_):
-          m = BooleanMetadata(x)
-        elif np.issubdtype(x.dtype, np.number):
-          m = NumericMetadata(x)
-        else:
-          m = LookupMetadata(x)
-        meta_kwargs[meta_name] = m
+    if len(meta_pkeys) > 0 and not np.array_equal(meta_pkeys, pkey):
+      return self._raise_error(415, 'Spectrum and metadata names mismatch.')
 
     # async loading machinery automatically registers us with DATASETS
     def _load(ds):
@@ -77,17 +110,37 @@ class DatasetImportHandler(BaseHandler):
       return True
 
     if ds_kind == 'LIBS':
-      if wave.shape != (6144,):
-        logging.error('wrong #chans for LIBS data: %s', wave.shape[0])
-        self.set_status(415)
-        return self.finish('Wrong number of channels for LIBS data.')
       WebLIBSDataset(ds_name, _load)
     else:
       WebVectorDataset(ds_name, ds_kind, _load)
+    return True
 
-    return self.write('/explorer?ds_kind=%s&ds_name=%s' % (
-        ds_kind, url_escape(ds_name, plus=False)))
+  def _load_metadata_csv(self):
+    # metadata is optional
+    if 'metadata' not in self.request.files:
+      return {}, []
 
+    f, = self.request.files['metadata']
+    fh = BytesIO(f['body'])
+    try:
+      meta = np.genfromtxt(fh, dtype=None, delimiter=',', names=True)
+      meta_pkeys = np.array(meta[meta.dtype.names[0]])
+    except Exception as e:
+      self._raise_error(415, 'Unable to parse metadata CSV.',
+                        'bad metadata file: %s' % e)
+      return None, None
+
+    meta_kwargs = {}
+    for meta_name in meta.dtype.names[1:]:
+      x = meta[meta_name]
+      if np.issubdtype(x.dtype, np.bool_):
+        m = BooleanMetadata(x)
+      elif np.issubdtype(x.dtype, np.number):
+        m = NumericMetadata(x)
+      else:
+        m = LookupMetadata(x)
+      meta_kwargs[meta_name] = m
+    return meta_kwargs, meta_pkeys
 
 routes = [
     (r'/_import', DatasetImportHandler),
