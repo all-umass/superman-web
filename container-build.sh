7af5cfbe388ab545f019da59bfacf7ace89d8285
Lex Miller <mille35a@mtholyoke.edu>
2022-09-16 13:02:33 -0400

    updated with python3-pywt

--- container-build.sh
+++ container-build.sh
@@ -3,7 +3,7 @@
 pip install --upgrade pip
 
 # TODO: manage these with apt instead; see README.md
-pip install h5py==2.10.0 matplotlib==3.1.3 pandas==1.0.0 PyWavelets==1.1.1 PyYAML==5.3 tornado==4.4.2
+pip install h5py==2.10.0 matplotlib==3.1.3 pandas==1.0.0 PyYAML==5.3 tornado==4.4.2
 
 # The superman package from PyPI is missing a file; this is the easiest way to install.
 cd ~
