4debe283b111770c4fc1bd1a36c2e1d9c4f031c6
Lex Miller <mille35a@mtholyoke.edu>
2022-09-19 09:22:11 -0400

    h5py version 2.10.0-9

--- lando/build.sh
+++ lando/build.sh
@@ -2,8 +2,6 @@
 
 pip install --upgrade pip
 
-# TODO: manage these with apt instead; see README.md
-pip install h5py==2.10.0 tornado==4.4.2
 
 # The superman package from PyPI is missing a file; this is the easiest way to install.
 cd ~
--- lando/build_as_root.sh
+++ lando/build_as_root.sh
@@ -6,3 +6,5 @@ apt install --yes python3-pywt
 apt install --yes python3-matplotlib 
 apt install --yes python3-pandas
 apt install --yes python3-yaml
+apt install -yes python3-tornado
+apt install -yes python3-h5py
